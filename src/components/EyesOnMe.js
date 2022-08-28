d7e9836179d3f4e3f3572529a9df4d4627e270ec
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-08-28 15:08:33 -0400

    commit tree 74915f99e1c62eacf0fe52aa7120409f4c918a1b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function handleB(){
+        return console.log('Hey! Eyes on me!')
+    }
+    function handleF(){
+        return console.log('Good!')
+    }
+    return( 
+        <div>
+            <button onBlur={handleB} onFocus={handleF}>Eyes on me</button>
+        </div>
+    );
+}
+
+export default EyesOnMe;
